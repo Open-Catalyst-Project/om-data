204e590949bde98e58d218660dfa2964da5657eb
Muhammed Shuaibi <mushuaibi@gmail.com>
2024-03-25 21:57:22 +0000

    add orca recipes

---
+++ omdata/orca/calc.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+from shutil import which
+
+from ase.calculators.orca import ORCA, OrcaProfile
+from pydantic import Field
+from sella import Sella
+
+ORCA_FUNCTIONAL = "wB97M-V"
+ORCA_BASIS = "def2-TZVPD"
+ORCA_SIMPLE_INPUT = [
+    "EnGrad",
+    "RIJCOSX",
+    "def2/J",
+    "NoUseSym",
+    "DIIS",
+    "NOSOSCF",
+    "NormalConv",
+    "DEFGRID3",
+]
+ORCA_SIMPLE_INPUT_QUACC_IGNORE = [
+    "#slowconv",
+]
+ORCA_BLOCKS = ["%scf Convergence Tight maxiter 500 end"]
+ORCA_ASE_SIMPLE_INPUT = " ".join([ORCA_FUNCTIONAL] + [ORCA_BASIS] + ORCA_SIMPLE_INPUT)
+OPT_PARAMETERS = {
+    "optimizer": Sella,
+    "store_intermediate_files": True,
+    "fmax": 0.01,
+    "max_steps": 1000,
+}
+
+
+def write_orca_inputs(
+    atoms,
+    output_directory,
+    charge=0,
+    mult=1,
+    orcasimpleinput=ORCA_ASE_SIMPLE_INPUT,
+    orcablocks=" ".join(ORCA_BLOCKS),
+):
+
+    MyOrcaProfile = OrcaProfile([Field(Path(which("orca") or "orca")).default])
+    calc = ORCA(
+        charge=charge,
+        mult=mult,
+        profile=MyOrcaProfile,
+        orcasimpleinput=orcasimpleinput,
+        orcablocks=orcablocks,
+        directory=output_directory,
+    )
+    calc.write_inputfiles(atoms, ["energy", "forces"])
