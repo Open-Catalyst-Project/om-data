efd5c49ac09d814f2a2732321835a49c8e318ed9
Brandon <bmwood@meta.com>
2024-04-16 20:31:53 +0000

    scripts for sampling geom

---
+++ biomolecules/geom/write_geom_drugs_structures.py
@@ -0,0 +1,92 @@
+import os
+import pickle
+import argparse
+import tqdm
+
+from rdkit.Chem.rdmolfiles import MolToXYZFile
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--base_path",
+        type=str,
+        required=True,
+        help="Path to where you untarred the rdkit folder i.e. path/to/rdkit_folder",
+    )
+    parser.add_argument(
+        "--parent_path",
+        type=str,
+        required=True,
+        help="Path to the parent directory where structures will be written",
+    )
+    parser.add_argument(
+        "--geom_ids_path",
+        type=str,
+        required=True,
+        help="Path to geom ids pkl to write structures for",
+    )
+    parser.add_argument(
+        "--idmol_path",
+        type=str,
+        required=True,
+        help="Path to id_mol_dict.pkl",
+    )
+    parser.add_argument(
+        "--start_idx",
+        type=int,
+        required=True,
+        help="Index in split ids to start writting files",
+    )
+    parser.add_argument(
+        "--stop_idx",
+        type=int,
+        required=True,
+        help="Index in split ids to stop writting files",
+    )
+
+    return parser.parse_args()
+
+
+def main():
+
+    args = parse_args()
+
+    # rdkit folder downloaded 2024-03-01
+    base_path = args.base_path
+    parent_path = args.parent_path
+    geom_ids_path = args.geom_ids_path
+    idmol_path = args.idmol_path
+    start_idx = args.start_idx
+    stop_idx = args.stop_idx
+
+    # read split ids
+    with open(geom_ids_path, "rb") as f:
+        geom_ids = pickle.load(f)
+
+    with open(idmol_path, "rb") as f:
+        idmol = pickle.load(f)
+
+    for i in tqdm.tqdm(geom_ids[start_idx:stop_idx]):
+        geom_id = str(i)
+        outdir = os.path.join(parent_path, geom_id)
+        if os.path.exists(outdir):
+            print(f"Directory {outdir} already exists, skipping")
+            continue
+        # make the directory
+        os.mkdir(outdir)
+        # load pickle file to get the molecule
+        pp = os.path.join(base_path, idmol[i]["pickle_path"])
+        with open(pp, "rb") as f:
+            mol_dict = pickle.load(f)
+        # get charge and conformer index
+        charge = idmol[i]["charge"]
+        conf_idx = idmol[i]["conf_idx"]
+        # multiplicity is always 1
+        f_name = os.path.join(outdir, f"structure_{charge}_1.xyz")
+        # write xyz file
+        MolToXYZFile(mol_dict["conformers"][conf_idx]["rd_mol"], f_name)
+
+
+if __name__ == "__main__":
+    main()
