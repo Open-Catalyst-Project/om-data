18a455b156f116d6f59078310fa7ba4f711fed2f
Daniel Levine <levineds@meta.com>
2024-05-03 11:52:29 -0700

    Add a function to determine the symmetry-breaking block in ORCA

--- omdata/orca/calc.py
+++ omdata/orca/calc.py
@@ -1,7 +1,10 @@
 from shutil import which
 
+from ase import Atoms
 from ase.calculators.orca import ORCA, OrcaProfile
 from sella import Sella
+
+ECP_SIZE = {**{i: 28 for i in range(37, 55)}, **{i: 46 for i in range(55, 58)}, **{i:28 for i in range(58, 72)}, **{i:60 for i in range(72, 87)}}
 
 ORCA_FUNCTIONAL = "wB97M-V"
 ORCA_BASIS = "def2-TZVPD"
@@ -36,14 +39,32 @@ OPT_PARAMETERS = {
     },
 }
 
+def get_symm_break_block(atoms: Atoms, charge: int)->str:
+    """
+    Determine the ORCA Rotate block needed to break symmetry in a singlet
+
+    This is determined by taking the sum of atomic numbers less any charge (because
+    electrons are negatively charged) and removing any electrons that are in an ECP
+    and dividing by 2. This gives the number of occupied orbitals, but since ORCA is
+    0-indexed, it gives the index of the LUMO.
+
+    We use a rotation angle of 20 degrees or about a 12% mixture of LUMO into HOMO.
+    This is somewhat arbitrary but similar to the default setting in Q-Chem, and seemed
+    to perform well in tests of open-shell singlets.
+    """
+    n_electrons = sum(atoms.get_atomic_numbers()) - charge
+    ecp_electrons = sum(ECP_SIZE.get(at_num, 0) for at_num in atoms.get_atomic_numbers())
+    n_electrons -= ecp_electrons
+    lumo = n_electrons // 2
+    return f"%scf rotate \{{lumo-1}, {lumo}, 20, 1, 1\} end end"
 
 def write_orca_inputs(
-    atoms,
+    atoms:Atoms,
     output_directory,
-    charge=0,
-    mult=1,
-    orcasimpleinput=ORCA_ASE_SIMPLE_INPUT,
-    orcablocks=" ".join(ORCA_BLOCKS),
+    charge:int=0,
+    mult:int=1,
+    orcasimpleinput:str=ORCA_ASE_SIMPLE_INPUT,
+    orcablocks:str=" ".join(ORCA_BLOCKS),
 ):
     """
     One-off method to be used if you wanted to write inputs for an arbitrary
