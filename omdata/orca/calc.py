5206c304588e82ae33d2b62a6c3c54742a6f875c
Muhammed Shuaibi <mushuaibi@gmail.com>
2024-05-06 19:20:12 +0000

    assume basis lives in same directory

--- omdata/orca/calc.py
+++ omdata/orca/calc.py
@@ -1,12 +1,17 @@
+from enum import Enum
 from shutil import which
-from pathlib import Path
 
 from ase import Atoms
 from ase.calculators.orca import ORCA, OrcaProfile
 from sella import Sella
 
 # ECP sizes taken from Table 6.5 in the Orca 5.0.3 manual
-ECP_SIZE = {**{i: 28 for i in range(37, 55)}, **{i: 46 for i in range(55, 58)}, **{i:28 for i in range(58, 72)}, **{i:60 for i in range(72, 87)}}
+ECP_SIZE = {
+    **{i: 28 for i in range(37, 55)},
+    **{i: 46 for i in range(55, 58)},
+    **{i: 28 for i in range(58, 72)},
+    **{i: 60 for i in range(72, 87)},
+}
 
 ORCA_FUNCTIONAL = "wB97M-V"
 ORCA_BASIS = "def2-TZVPD"
@@ -26,8 +31,8 @@ ORCA_BLOCKS = [
     "%scf Convergence Tight maxiter 300 end",
     "%elprop Dipole true Quadrupole true end",
     '%nbo NBOKEYLIST = "$NBO NPA NBO E2PERT 0.1 $END" end',
-    '%output Print[P_ReducedOrbPopMO_L] 1 Print[P_ReducedOrbPopMO_M] 1 Print[P_BondOrder_L] 1 Print[P_BondOrder_M] 1 end',
-    f"%basis GTOName \"{str(Path(__file__).resolve().parent / 'def2-tzvpd.bas')}\" end"
+    "%output Print[P_ReducedOrbPopMO_L] 1 Print[P_ReducedOrbPopMO_M] 1 Print[P_BondOrder_L] 1 Print[P_BondOrder_M] 1 end",
+    '%basis GTOName "def2-tzvpd.bas" end',
 ]
 ORCA_ASE_SIMPLE_INPUT = " ".join([ORCA_FUNCTIONAL] + [ORCA_BASIS] + ORCA_SIMPLE_INPUT)
 OPT_PARAMETERS = {
@@ -41,11 +46,13 @@ OPT_PARAMETERS = {
     },
 }
 
-class Vertical(Enum):
-    Default = 'default'
-    MetalOrganics = 'metal-organics'
 
-def get_symm_break_block(atoms: Atoms, charge: int)->str:
+class Vertical(Enum):
+    Default = "default"
+    MetalOrganics = "metal-organics"
+
+
+def get_symm_break_block(atoms: Atoms, charge: int) -> str:
     """
     Determine the ORCA Rotate block needed to break symmetry in a singlet
 
@@ -59,19 +66,22 @@ def get_symm_break_block(atoms: Atoms, charge: int)->str:
     to perform well in tests of open-shell singlets.
     """
     n_electrons = sum(atoms.get_atomic_numbers()) - charge
-    ecp_electrons = sum(ECP_SIZE.get(at_num, 0) for at_num in atoms.get_atomic_numbers())
+    ecp_electrons = sum(
+        ECP_SIZE.get(at_num, 0) for at_num in atoms.get_atomic_numbers()
+    )
     n_electrons -= ecp_electrons
     lumo = n_electrons // 2
     return f"%scf rotate {{{lumo-1}, {lumo}, 20, 1, 1}} end end"
 
+
 def write_orca_inputs(
-    atoms:Atoms,
+    atoms: Atoms,
     output_directory,
-    charge:int=0,
-    mult:int=1,
-    orcasimpleinput:str=ORCA_ASE_SIMPLE_INPUT,
-    orcablocks:str=" ".join(ORCA_BLOCKS),
-    vertical=Vertical.Default
+    charge: int = 0,
+    mult: int = 1,
+    orcasimpleinput: str = ORCA_ASE_SIMPLE_INPUT,
+    orcablocks: str = " ".join(ORCA_BLOCKS),
+    vertical=Vertical.Default,
 ):
     """
     One-off method to be used if you wanted to write inputs for an arbitrary
@@ -80,8 +90,8 @@ def write_orca_inputs(
 
     MyOrcaProfile = OrcaProfile([which("orca")])
     if vertical == Vertical.MetalOrganics and mult == 1:
-        orcasimpleinput += ' UKS'
-        orcablocks += f' {get_symm_break_block(atoms, charge)}'
+        orcasimpleinput += " UKS"
+        orcablocks += f" {get_symm_break_block(atoms, charge)}"
 
     calc = ORCA(
         charge=charge,
