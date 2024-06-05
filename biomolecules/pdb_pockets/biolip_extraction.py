6d327fc2f8172f982a5df0fda9b84ed6326d7526
Daniel Levine <levineds@meta.com>
2024-06-05 16:53:45 +0000

    Refactor

--- biomolecules/pdb_pockets/biolip_extraction.py
+++ biomolecules/pdb_pockets/biolip_extraction.py
@@ -15,7 +15,19 @@ from schrodinger.adapter import evaluate_smarts
 SCHRO = "/opt/schrodinger2024-1"
 
 
-def get_biolip_db(lig_type="reg"):
+class OnlyCAError(Exception):
+    pass
+
+
+class MissingAtomsError(Exception):
+    pass
+
+
+class MissingResiduesError(Exception):
+    pass
+
+
+def get_biolip_db(lig_type: str = "reg") -> pd.DataFrame:
     columns = [
         "pdb_id",
         "receptor_chain",
@@ -85,22 +97,39 @@ def get_biolip_db(lig_type="reg"):
     return biolip_df
 
 
-def retreive_ligand_and_env(biolip_df, ligand_size_limit=50, start_pdb=0, end_pdb=1000):
-    random.seed(12341)
+def retreive_ligand_and_env(
+    biolip_df: pd.DataFrame,
+    ligand_size_limit: int = 50,
+    start_pdb: int = 0,
+    end_pdb: int = 1000,
+) -> None:
+    """
+    Extract ligand and its environment from BioLiP examples
+
+    Procedure is as follows, for each protein in BioLiP:
+        1) Download the structure and prepare it with PrepWizard (re-using already prepared proteins if possible)
+        2) Obtain list of atoms which are assigned as ligands or receptors in BioLiP
+        3) Extract the receptor residues and cap them to match the source protein
+        4) Write the extracted structure to a pdb file
+    """
+    # random.seed(12341)
     grouped_biolip = biolip_df.groupby("pdb_id")
+    pdb_list = list(grouped_biolip.groups.keys())
     for pdb_count in range(start_pdb, end_pdb):
         # pdb_id = random.choice(list(grouped_biolip.groups.keys()))
-        pdb_id = list(grouped_biolip.groups.keys())[pdb_count]
+        pdb_id = pdb_list[pdb_count]
         rows = grouped_biolip.get_group(pdb_id)
         print(f"preparing {pdb_id} (entry: {pdb_count})")
         sys.stdout.flush()
         prepped_pdb_fnames = set()
         for idx, row in rows.iterrows():
+            binding_site_counter = (
+                row["binding_site_number_code"] + row["receptor_chain"]
+            )
             try:
                 st, fname = get_prepped_protein(pdb_id, row)
-            except ValueError as e:
+            except OnlyCAError:
                 print(f"Error on {pdb_id}: Structure has only CA")
-                print(e)
                 continue
             except RuntimeError as e:
                 print(f"Error on {pdb_id}: PrepWizard failed")
@@ -108,19 +137,20 @@ def retreive_ligand_and_env(biolip_df, ligand_size_limit=50, start_pdb=0, end_pd
                 continue
             else:
                 prepped_pdb_fnames.add(fname)
-            binding_site_counter = (
-                row["binding_site_number_code"] + row["receptor_chain"]
-            )
+
             try:
                 lig_ats, res_ats = get_atom_lists(st, row)
-            except RuntimeError as e:
+            except MissingAtomsError:
                 print(f"Error on {pdb_id}, BS{binding_site_counter}: Atoms are missing")
-                print(e)
                 continue
+
+            # Count heavy atoms of ligand and skip if too large
             heavy_lig_ats = [at for at in lig_ats if st.atom[at].atomic_number > 1]
             if len(heavy_lig_ats) > ligand_size_limit:
                 print("ligand too big: ", len(heavy_lig_ats))
                 continue
+
+            # Do the extraction
             print(f"obtaining {pdb_id}, binding site {binding_site_counter}")
             ligand_env = st.extract(lig_ats + res_ats)
             try:
@@ -138,6 +168,34 @@ def retreive_ligand_and_env(biolip_df, ligand_size_limit=50, start_pdb=0, end_pd
         # Cleanup the remaining prepped files for this pdb_id
         for fname in prepped_pdb_fnames:
             os.remove(fname)
+
+
+def download_cif(pdb_id, chains):
+    subprocess.run(
+        [os.path.join(SCHRO, "utilities", "getpdb"), pdb_id, "-format", "cif"]
+    )
+    st = StructureReader.read(f"{pdb_id}.cif")
+    # only extract the relevant chains because these proteins tend to be very large and PrepWizard takes hours
+    at_list = []
+    for chain in chains:
+        at_list.extend(st.chain[chain].getAtomList())
+    st = st.extract(at_list)
+    fname = f"{pdb_id}.maegz"
+    with StructureWriter(fname) as writer:
+        writer.append(st)
+    return fname
+
+
+def missing_backbone_check(st):
+    if (
+        sum(
+            1
+            for mol in st.molecule
+            if len(mol.atom) == 1 and mol.atom[1].element == "C"
+        )
+        > 20
+    ):
+        raise OnlyCAError
 
 
 def get_prepped_protein(pdb_id, row, prep=True):
@@ -167,46 +225,23 @@ def get_prepped_protein(pdb_id, row, prep=True):
         subprocess.run([os.path.join(SCHRO, "utilities", "getpdb"), pdb_name])
         if not os.path.exists(fname):
             # PDB could not be downloaded, try the cif
-            subprocess.run(
-                [os.path.join(SCHRO, "utilities", "getpdb"), pdb_id, "-format", "cif"]
-            )
-            st = StructureReader.read(f"{pdb_id}.cif")
-            # only extract the relevant chains because these proteins tend to be very large and PrepWizard takes hours
-            at_list = []
-            for chain in chains:
-                at_list.extend(st.chain[chain].getAtomList())
-            st = st.extract(at_list)
-            fname = f"{pdb_id}.maegz"
-            with StructureWriter(fname) as writer:
-                writer.append(st)
+            fname = download_cif(pdb_id, chains)
         st = StructureReader.read(fname)
         # Reject structure that are just a bunch of CA
-        if (
-            sum(
-                1
-                for mol in st.molecule
-                if len(mol.atom) == 1 and mol.atom[1].element == "C"
-            )
-            > 20
-        ):
-            raise ValueError
+        missing_backbone_check(st)
+
+        # Remove any dummy atoms, PrepWizard doesn't like them
         dummy_atoms = [at for at in st.atom if at.atomic_number < 1]
         st.deleteAtoms(dummy_atoms)
         st.write(fname)
-        subprocess.run(
-            [
-                os.path.join(SCHRO, "utilities", "prepwizard"),
-                fname,
-                outname,
-                "-noepik",
-                "-noimpref",
-                "-fillsidechains",
-                "-disulfides",
-                "-NOJOBID",
-            ]
-        )
+
+        # Run PrepWizard
+        try:
+            run_prepwizard(fname, outname)
+        except subprocess.TimeoutExpired:
+            raise RuntimeError('PrepWizard took longer than 2 hours, skipping')
     if not os.path.exists(outname):
-        raise RuntimeError
+        raise RuntimeError('PrepWizard failed')
 
     st = StructureReader.read(outname)
     deprotonate_phosphate_esters(st)
@@ -220,6 +255,38 @@ def get_prepped_protein(pdb_id, row, prep=True):
         shutil.rmtree(deldir)
     return st, outname
 
+def run_prepwizard(fname, outname):
+    try:
+        subprocess.run(
+            [
+                os.path.join(SCHRO, "utilities", "prepwizard"),
+                fname,
+                outname,
+                "-noepik",
+                "-noimpref",
+                "-fillsidechains",
+                "-disulfides",
+                "-NOJOBID",
+            ],
+            timeout=3600*2 # Wait not more than 2 hours
+        )
+    except subprocess.TimeoutExpired as e:
+        # Try again without PROPKA (which seems to be where things get stuck)
+        subprocess.run(
+            [
+                os.path.join(SCHRO, "utilities", "prepwizard"),
+                fname,
+                outname,
+                "-noepik",
+                "-noimpref",
+                "-fillsidechains",
+                "-disulfides",
+                "-nopropka",
+                "-NOJOBID",
+            ],
+            timeout=3600*2 # Wait not more than 2 hours
+        )
+        
 
 def deprotonate_phosphate_esters(st):
     """
@@ -235,7 +302,8 @@ def deprotonate_phosphate_esters(st):
         st.atom[O_at].formal_charge -= 1
     st.deleteAtoms(H_ats)
 
-def make_gaps_gly(st, gap_res):
+
+def make_gaps_gly(st, row, gap_res):
     """
     Turn gap residues into glycines
 
@@ -265,10 +333,8 @@ def make_gaps_gly(st, gap_res):
         # and it will actually also garble the structure
         if res.getBetaCarbon() is not None:
             st.deleteBond(res.getBetaCarbon(), res.getAlphaCarbon())
-        try:
-            build.mutate(st, res.getAlphaCarbon(), "GLY")
-        except:
-            continue
+        build.mutate(st, res.getAlphaCarbon(), "GLY")
+
 
 def get_atom_lists(st, row, include_waters=False):
     """
@@ -281,15 +347,15 @@ def get_atom_lists(st, row, include_waters=False):
     res_ats = []
     res_list = [res[1:] for res in row["binding_site_residues_pdb"].split()]
     gap_res = get_single_gaps(st, row["receptor_chain"], res_list)
-    make_gaps_gly(st, gap_res)
+    make_gaps_gly(st, row, gap_res)
 
-    # Mutating residues can change the atom numbering so out of an abundance
-    # of caution, let's retreive all the atom indices in a separate loop
+    # Mutating residues can change the atom numbering so let's retreive
+    # all the atom indices in a separate loop
     for res_num in gap_res + res_list:
         res = st.findResidue(f'{row["receptor_chain"]}:{res_num}')
         if res.hasMissingAtoms():
             print(f'Missing atoms in rec res {row["receptor_chain"]}:{res_num}')
-            raise RuntimeError
+            raise MissingAtomsError
         res_ats.extend(res.getAtomIndices())
 
     lig_ats = []
@@ -297,7 +363,7 @@ def get_atom_lists(st, row, include_waters=False):
         res_name = f'{row["ligand_chain"]}:{res_num}{row["ligand_residue_inscode"]}'
         try:
             res = st.findResidue(res_name)
-        except ValueError:
+        except MissingResiduesError:
             print(f"missing expected residue: {res_name}")
             continue
         lig_ats.extend(res.getAtomIndices())
@@ -309,7 +375,7 @@ def get_atom_lists(st, row, include_waters=False):
 
     # mark the ligand chain
     for at in lig_ats:
-        st.atom[at].chain = "ligand"
+        st.atom[at].chain = "l"
     # mark the receptor chain
     for at in res_ats:
         st.atom[at].chain = "A"
@@ -318,6 +384,21 @@ def get_atom_lists(st, row, include_waters=False):
 
 
 def get_single_gaps(st, rec_chain, res_list):
+    """
+    Get residues that are not in a list of residues but are
+    between two residues which are in the list.
+
+    If we have a single residue which would not normally be included
+    which is between two which are, then the capping groups of those
+    two are going to overlap because they are both trying to fill in
+    the same missing CA (the CA of the gap residue). We need to treat
+    these specially so we retrieve them here.
+
+    :param st: structure to analyze
+    :param rec_chain: name of receptor chain
+    :param res_list: list of residue names which may have single-residue gaps
+    :return: list of residues which are single-residue gaps in `res_list`
+    """
     parent_indices = set()
     parent_list = list(st.chain[rec_chain].residue)
     for res_name in res_list:
@@ -333,6 +414,13 @@ def get_single_gaps(st, rec_chain, res_list):
 
 
 def cap_termini(st, ligand_env):
+    """
+    Cap termini for extracted receptor chains with NMA or ACE oriented to match
+    the positions of neighboring residues in the original structure.
+
+    :param st: original protein structure from which the pocket was extracted
+    :param ligand_env: extracted protein receptor (i.e. ligand environment)
+    """
     capterm = CapTermini(ligand_env, verbose=False, frag_min_atoms=0)
     for res in capterm.cappedResidues():
         orig_res = st.findResidue(res)
