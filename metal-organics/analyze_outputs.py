f36e4508c468dd5a05dd06fcdbcced99e9647ee6
Daniel Levine <levineds@meta.com>
2024-06-18 22:06:19 +0000

    improve xyz generation

--- metal-organics/analyze_outputs.py
+++ metal-organics/analyze_outputs.py
@@ -4,6 +4,7 @@ import pathlib
 
 import pandas as pd
 from architector import convert_io_molecule
+from tqdm import tqdm
 
 
 def parse_args():
@@ -15,8 +16,10 @@ def parse_args():
 def main():
     args = parse_args()
     path = pathlib.Path(args.output_path)
-    for fname in path.glob("*.pkl"):
-        name = os.path.join("xyzs", os.path.splitext(os.path.basename(fname))[0])
+    xyz_path = path / "xyzs"
+    xyz_path.mkdir(exist_ok=True)
+    for fname in tqdm(path.glob("*.pkl"), total=len(list(path.glob("*.pkl")))):
+        name = xyz_path / os.path.splitext(os.path.basename(fname))[0]
         df = pd.read_pickle(fname)
         for i, row in df.iterrows():
             mol = convert_io_molecule(row["mol2string"])
