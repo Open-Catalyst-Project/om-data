30ed02c230e5a236bd33487c2b31c091f768dbf0
Sanjeev Raja <sanjeevr@germain.lbl.gov>
2024-06-27 10:00:25 -0700

    trying to handle multi-atom solutes, committing before migrating to schrodinger

--- electrolytes/run_extraction.sh
+++ electrolytes/run_extraction.sh
@@ -5,15 +5,23 @@
 # python solvation_shell_extract.py --pdb_file_path 'testfiles/al_clo4_example.pdb' \
 #                                   --save_dir 'results' \
 #                                   --system_name 'Al_ClO4' \
-#                                   --solute_atoms 'AL0' \
+#                                   --solute_atoms 'C' \
 #                                   --min_coord 2 \
 #                                   --max_coord 5 \
 #                                   --top_n 20
 
-python solvation_shell_extract.py --input_dir 'testfiles/NaCl' \
+# python solvation_shell_extract.py --input_dir 'testfiles/LiPF6' \
+#                                   --save_dir 'results' \
+#                                   --system_name 'LiPF6' \
+#                                   --solute_atoms 'P00' 'F01' 'F02' 'F03' 'F04' 'F05' 'F06'  \
+#                                   --min_coord 2 \
+#                                   --max_coord 20 \
+#                                   --top_n 20
+
+python solvation_shell_extract.py --input_dir 'testfiles/Al_ClO4' \
                                   --save_dir 'results' \
-                                  --system_name 'NaCl' \
-                                  --solute_atoms 'NA0'  \
+                                  --system_name 'Al_ClO4' \
+                                  --solute_atoms 'CL1' 'O00' 'O02' 'O03' 'O04'  \
                                   --min_coord 2 \
-                                  --max_coord 5 \
+                                  --max_coord 20 \
                                   --top_n 20
--- electrolytes/solvation_shell_extract.py
+++ electrolytes/solvation_shell_extract.py
@@ -4,6 +4,7 @@ import argparse
 import random
 import numpy as np
 from tqdm import tqdm
+from typing import List
 from collections import defaultdict
 import matplotlib.pyplot as plt
 import MDAnalysis as mda
@@ -15,13 +16,15 @@ from solvation_shell_utils import (
     wrap_positions,
     extract_charges_from_lammps_file,
 )
+from utils import parse_list_of_lists
 
 
 def extract_solvation_shells(
     input_dir: str,
     save_dir: str,
     system_name: str,
-    solute_atoms: str,
+    solute_atoms: List[str],
+    solvent_atoms: List[List[str]],
     min_coord: int,
     max_coord: int,
     top_n: int,
@@ -32,12 +35,13 @@ def extract_solvation_shells(
     extract and save the top_n most diverse snapshots based on a RMSD criterion.
 
     Args:
-        pdb_file_path: Path to the PDB file containing the MD trajectory
-        save_dir: Directory in which to save extracted solvation shells
-        system_name: Name of the system - used for naming the save directory
-        solute_atom: Name (in the PDB file) of the solute atom type (e.g NA0) with which to perform the solvation analysis
-        min_coord: Minimum coordination number to consider
-        max_coord: Maximum coordination number to consider
+        input_dir: Path to 1) the PDB file containing the MD trajectory and 2) the LAMMPS file containing the initial structure (used to extract partial charges).
+        save_dir: Directory in which to save extracted solvation shells.
+        system_name: Name of the system - used for naming the save directory.
+        solute_atoms: List of names (in the PDB file) of the solute atom types (e.g NA0) with which to perform the solvation analysis.
+        solvent_atoms: List of names (in the PDB file) of the solvent atom types with which to perform the solvation analysis.
+        min_coord: Minimum coordination number to consider.
+        max_coord: Maximum coordination number to consider.
         top_n: Number of snapshots to extract per coordination number.
     """
     solute_atoms_name = " ".join(solute_atoms)
@@ -64,93 +68,138 @@ def extract_solvation_shells(
 
     # Choose solute atom
     # TODO: try to join multiple atoms in the same solute, doesn't currently work
-    query = " or ".join(f"name {atom}" for atom in solute_atoms)
-    solu = universe.select_atoms(query)
+    solute_query = " or ".join(f"name {atom}" for atom in solute_atoms)
+    solute = universe.select_atoms(solute_query)
+    # other_query = " or ".join(f"name {atom}" for atom in other_solute_atoms)
+    # other_solu = universe.select_atoms(other_query)
 
     logging.info("Translating atoms to solute center of mass")
     for ts in tqdm(universe.trajectory):
         ts.dimensions = universe.dimensions
-        solu_center = solu.center_of_mass(wrap=True)
+        solu_center = solute.center_of_mass(wrap=True)
         dim = ts.triclinic_dimensions
         box_center = np.sum(dim, axis=0) / 2
         universe.atoms.translate(box_center - solu_center)
 
         universe.atoms.unwrap()
 
-    solvent = universe.atoms - solu
+    # TODO: need to remove the other components of the solute from the solvent atoms? Doesn't matter since the other solute atom won't be close?
 
-    solv_anal = Solute.from_atoms(solu, {"solvent": solvent})
+    # This doesn't handle multiple solvents
+    solvent = (
+        universe.atoms - solute
+    )  # TODO: might need to count anything other than the solute of interest as a potential solvent
+    # TODO: set an upper cutoff for solute-solvent interactions
 
-    # Identify the cutoff for the first solvation shell, based on the MD trajectory
+    solute_name = "".join(atom for atom in solute_atoms)
+
     logging.info("Running solvation analysis")
+    # Identify the cutoff for the first solvation shell, based on the MD trajectory
 
-    # this line fails (can't find the radius)
-    solv_anal.run()
+    # discovered that the reason the analysis was failing was because the default cutoff region of 6.5 was too small for the Cl04 anion
+    kernel_kwargs = {"cutoff_region": (1.5, 8)}
+    solv_anal = Solute.from_atoms(
+        solute,
+        {"solvent": solvent},
+        solute_name=solute_name,
+        kernel_kwargs=kernel_kwargs,
+    )
+    # TODO: subsample fewer solutes to speed up the analysis
+
+    solv_anal.run()  # which solute's radii is this using?
+
+    max_radii = 0
+    max_name = ""
+    for solute_name in solv_anal.atom_solutes.keys():
+        radii = solv_anal.atom_solutes[solute_name].radii["solvent"]
+        if radii > max_radii:
+            max_radii = radii
+            max_name = solute_name
+
+    # TODO: what do we do with this max or mean radii? Can we need to use it to re-run the analysis?
+
+    # except AssertionError:
+    #     logging.info("Failed to automatically find the radius, trying again with a heuristic radius")
+    #     heuristic_radii = 2.5 # TODO: add heuristic radii based on effective sizes of solute and solvent
+    #     solv_anal = Solute.from_atoms(solu, {"solvent": solvent}, solute_name = solute_name, radii={'solvent': heuristic_radii})
+    #     solv_anal.run()
 
     # Plot the RDF
-    solv_anal.plot_solvation_radius("solute", "solvent")
+    solv_anal.plot_solvation_radius(max_name, "solvent")
     plt.savefig(
         os.path.join(save_dir, system_name, solute_atoms_name, "solvation_rdf.png")
     )
 
     # There's probably a much faster way to do this
     # But for now, we're prototyping, so slow is okay
+
+    # Store the solvation shells with different numbers of solvents (sorted from highest to lowest frequency)
     shells = dict()
     for j in solv_anal.speciation.speciation_fraction["solvent"]:
         shells[j] = solv_anal.speciation.get_shells({"solvent": j})
 
     # Now let's try getting the most diverse structures for each particular coordination number
     # This is also a bit slow, particularly for the more common and/or larger solvent shells
+
+    # TODO: instead of specifying a range of coordination numbers, we could specify the top n most common coordination numbers (but we also probably want to extract rare configurations)
     for c in range(min_coord, max_coord + 1):
-        logging.info(f"Processing shells with coordination number {c}")
-        os.makedirs(
-            os.path.join(save_dir, system_name, solute_atoms_name, f"coord={c}"),
-            exist_ok=True,
-        )
-        shell_species = []
-        shell_positions = []
-        for index, _ in tqdm(shells[c].iterrows()):
-            ts = universe.trajectory[index[0]]
-            universe.atoms.unwrap()
-            shell = solv_anal.solvation_data.xs(
-                (index[0], index[1]), level=(FRAME, SOLUTE_IX)
+        if c in shells.keys():
+            logging.info(
+                f"Processing {len(shells[c])} shells with coordination number {c}"
             )
-            shell = solv_anal._df_to_atom_group(shell, solute_index=index[1])
-            shell = shell.copy()
-            if len(shell.atoms.elements) > len(shell_species):
-                shell_species = shell.atoms.elements
+            os.makedirs(
+                os.path.join(save_dir, system_name, solute_atoms_name, f"coord={c}"),
+                exist_ok=True,
+            )
+            shell_species = []
+            shell_positions = []
+            # Extract the shell positions
 
-            shell_positions.append(wrap_positions(shell.atoms.positions, lattices))
+            for i, (index, _) in tqdm(enumerate(shells[c].iterrows())):
+                universe.atoms.unwrap()
+                frame, solute_idx = index
+                shell = solv_anal.solvation_data.xs(
+                    (frame, solute_idx), level=(FRAME, SOLUTE_IX)
+                )
+                shell = solv_anal._df_to_atom_group(shell, solute_index=index[1])
+                # TODO: assertion that compositions/element orderings are the same
+                if len(shell.atoms.elements) > len(shell_species):
+                    shell_species = shell.atoms.elements
+                shell_positions.append(wrap_positions(shell.atoms.positions, lattices))
+                if i > 1000:  # TODO: don't hardcode this
+                    break
 
-        by_num_atoms = defaultdict(list)
-        for sps in shell_positions:
-            by_num_atoms[len(sps)].append(sps)
+            by_num_atoms = defaultdict(list)
+            for sps in shell_positions:
+                by_num_atoms[len(sps)].append(sps)
 
-        # filter by number of atoms per shell
-        selections_by_num_atoms = {
-            num_atoms: filter_by_rmsd(shells_with_num_atoms, top_n)
-            for num_atoms, shells_with_num_atoms in by_num_atoms.items()
-        }
+            # filter by number of atoms per shell
+            selections_by_num_atoms = {
+                num_atoms: filter_by_rmsd(shells_with_num_atoms, top_n)
+                for num_atoms, shells_with_num_atoms in by_num_atoms.items()
+            }
 
-        for (
-            shell_size,
-            shell_positions,
-        ) in selections_by_num_atoms.items():  # loop over sizes
-            for idx, shell_pos in enumerate(shell_positions):
-                if shell_pos.shape[0] == shell_species.shape[0]:
+            for (
+                shell_size,
+                shell_positions,
+            ) in selections_by_num_atoms.items():  # loop over sizes
+                for idx, shell_pos in enumerate(shell_positions):
+                    if shell_pos.shape[0] == shell_species.shape[0]:
 
-                    # Save shell as xyz file
-                    mol = Molecule(shell_species, shell_pos, charge=round(sum(charges)))
-                    mol.to(
-                        os.path.join(
-                            save_dir,
-                            system_name,
-                            solute_atoms_name,
-                            f"coord={c}",
-                            f"size{shell_size}_selection{idx}.xyz",
-                        ),
-                        "xyz",
-                    )
+                        # Save shell as xyz file
+                        mol = Molecule(
+                            shell_species, shell_pos, charge=round(sum(charges))
+                        )
+                        mol.to(
+                            os.path.join(
+                                save_dir,
+                                system_name,
+                                solute_atoms_name,
+                                f"coord={c}",
+                                f"size{shell_size}_selection{idx}.xyz",
+                            ),
+                            "xyz",
+                        )
 
 
 if __name__ == "__main__":
@@ -169,10 +218,16 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--solute_atoms",
-        type=str,
-        nargs="+",
-        help="Which solute atom(s) to extract solvation shells for (accepts multiple values)",
+        type=parse_list_of_lists,
+        help="Which solute atom(s) to extract solvation shells for (accepts a list of lists, where each list is separated by a semicolon: e.g., 'a,b,c;d,e,f')",
     )
+
+    parser.add_argument(
+        "--solvent_atoms",
+        type=parse_list_of_lists,
+        help="Which solvent atom(s) to extract solvation shells for (accepts a list of lists, where each list is separated by a semicolon: e.g., 'a,b,c;d,e,f')",
+    )
+
     parser.add_argument(
         "--min_coord", type=int, help="Minimum shell coordination number to extract"
     )
@@ -193,6 +248,7 @@ if __name__ == "__main__":
         args.save_dir,
         args.system_name,
         args.solute_atoms,
+        args.solvent_atoms,
         args.min_coord,
         args.max_coord,
         args.top_n,
