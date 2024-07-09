460180dbf416cb2b2953619e6c57193a017eb0fb
Muhammad Risyad Hasyim <mh7373@system76-pc.localdomain>
2024-07-09 11:33:02 -0400

    Edit run scripts to read the temperature from the CSV file.

--- electrolytes/runsolvent.py
+++ electrolytes/runsolvent.py
@@ -3,29 +3,36 @@ from openmm import *
 from openmm.unit import *
 from sys import stdout, exit
 from pathlib import Path
+import csv 
 
 #TO-DO: Read temperature from CSV file
 pdb_initfile = 'solvent_init.pdb'
 ff_xml = 'solvent.xml'
 if Path(pdb_initfile).is_file() and Path(ff_xml).is_file():
+    row_idx  = int(sys.argv[1]) + 1
+    # Load the CSV file containing systems to simulate
+    with open("../elytes.csv", "r") as f:
+        systems = list(csv.reader(f))
+    Temp = float(systems[row_idx][4])
     pdb = app.PDBFile(pdb_initfile)
     modeller = app.Modeller(pdb.topology, pdb.positions)
     forcefield = app.ForceField(ff_xml)
     system = forcefield.createSystem(modeller.topology, nonbondedMethod=PME, nonbondedCutoff=0.5, constraints=None)
-    system.addForce(MonteCarloBarostat(1.0*bar, 293*kelvin, 50))
-    integrator = LangevinMiddleIntegrator(293.00*kelvin,   # Temperate of head bath
+    system.addForce(MonteCarloBarostat(1.0*bar, Temp*kelvin, 1))
+    integrator = LangevinMiddleIntegrator(Temp*kelvin,   # Temperate of head bath
                                           1/picosecond, # Friction coefficient
                                           0.002*picoseconds) # Time step
     simulation = app.Simulation(modeller.topology, system, integrator)
     simulation.context.setPositions(modeller.positions)
-    simulation.minimizeEnergy()
-    frames = 200
-    runtime = 50000
+    simulation.minimizeEnergy(maxIterations=1000)
+    frames = 1000
+    runtime = 10000
     rate = int(runtime/frames)
     if rate == 0:
         rate = 1
 
     simulation.reporters.append(StateDataReporter('solventdata.txt', rate, progress=True, temperature=True, potentialEnergy=True, density=True,totalSteps=runtime,speed=True))
+    simulation.reporters.append(PDBReporter('solvent_output.pdb', rate, enforcePeriodicBox=True))
     simulation.step(runtime)
 else:
     print(f'Either {pdb_initfile} or {ff_xml} are missing. Not an error but check if the system is molten salt/ionic liquid')
--- electrolytes/runsystem.py
+++ electrolytes/runsystem.py
@@ -2,21 +2,27 @@ from openmm.app import *
 from openmm import *
 from openmm.unit import *
 from sys import stdout, exit
+import csv
 
+row_idx  = int(sys.argv[1]) + 1
+# Load the CSV file containing systems to simulate
+with open("../elytes.csv", "r") as f:
+    systems = list(csv.reader(f))
+Temp = float(systems[row_idx][4])
 
 pdb = app.PDBFile('system_init.pdb')
 modeller = app.Modeller(pdb.topology, pdb.positions)
 forcefield = app.ForceField('system.xml')
 system = forcefield.createSystem(modeller.topology, nonbondedMethod=PME, nonbondedCutoff=0.5, constraints=None)
-system.addForce(MonteCarloBarostat(1.0*bar, 293*kelvin, 50))
-integrator = LangevinMiddleIntegrator(293.00*kelvin,   # Temperate of head bath
+system.addForce(MonteCarloBarostat(1.0*bar, Temp*kelvin, 1))
+integrator = LangevinMiddleIntegrator(Temp*kelvin,   # Temperate of head bath
                                       1/picosecond, # Friction coefficient
                                       0.002*picoseconds) # Time step
 simulation = app.Simulation(modeller.topology, system, integrator)
 simulation.context.setPositions(modeller.positions)
-simulation.minimizeEnergy()
+simulation.minimizeEnergy(maxIterations=1000)
 frames = 1000
-runtime = 1#5000000
+runtime = 100000
 
 rate = int(runtime/frames)
 if rate < 1:
