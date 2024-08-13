a477adae7564aa0655b030ef027166a73182b540
Daniel Levine <levineds@meta.com>
2024-08-13 17:59:41 +0000

    Revert "merge main"
    This reverts commit 2cc579340b08a317b194515c831eb4147960bca2, reversing
    changes made to 61287283a5f299c71b19724cc0c9c7ea94fc61e8.

--- omdata/orca/recipes.py
+++ omdata/orca/recipes.py
@@ -1,19 +1,16 @@
-from __future__ import annotations
-
 import os
+import pickle
 
-import psutil
-from quacc.recipes.orca.core import run_and_summarize, run_and_summarize_opt
+from quacc.recipes.orca.core import ase_relax_job, static_job
+from quacc.wflow_tools.customizers import strip_decorator
 
 from omdata.orca.calc import (
     OPT_PARAMETERS,
     ORCA_BASIS,
     ORCA_BLOCKS,
-    NBO_FLAGS,
     ORCA_FUNCTIONAL,
     ORCA_SIMPLE_INPUT,
-    Vertical,
-    get_symm_break_block,
+    ORCA_SIMPLE_INPUT_QUACC_IGNORE,
 )
 
 
@@ -23,14 +20,10 @@ def single_point_calculation(
     spin_multiplicity,
     xc=ORCA_FUNCTIONAL,
     basis=ORCA_BASIS,
-    orcasimpleinput=None,
-    orcablocks=None,
+    orcasimpleinput=ORCA_SIMPLE_INPUT,
+    orcablocks=ORCA_BLOCKS,
     nprocs=12,
     outputdir=os.getcwd(),
-    vertical=Vertical.Default,
-    nbo=False,
-    copy_files=None,
-    **calc_kwargs,
 ):
     """
     Wrapper around QUACC's static job to standardize single-point calculations.
@@ -56,46 +49,27 @@ def single_point_calculation(
         List of `orcablocks` swaps for the calculator
     nprocs: int
         Number of processes to parallelize across
-    nbo: bool
-        Run NBO as part of the Orca calculation
     outputdir: str
         Directory to move results to upon completion
-    calc_kwargs:
-        Additional kwargs for the custom Orca calculator
     """
     from quacc import SETTINGS
 
     SETTINGS.RESULTS_DIR = outputdir
 
-    if orcasimpleinput is None:
-        orcasimpleinput = ORCA_SIMPLE_INPUT.copy()
-    if orcablocks is None:
-        orcablocks = ORCA_BLOCKS.copy()
-    if vertical == Vertical.MetalOrganics and spin_multiplicity == 1:
-        orcasimpleinput.append("UKS")
-        orcablocks.append(get_symm_break_block(atoms, charge))
-    if not nbo:
-        orcasimpleinput.extend(["NONBO", "NONPA"])
-    else:
-        orcablocks.append(NBO_FLAGS)
-
-    nprocs = psutil.cpu_count(logical=False) if nprocs == "max" else nprocs
-    default_inputs = [xc, basis, "engrad"]
-    default_blocks = [f"%pal nprocs {nprocs} end"]
-
-    doc = run_and_summarize(
+    o = strip_decorator(static_job)(
         atoms,
         charge=charge,
         spin_multiplicity=spin_multiplicity,
-        default_inputs=default_inputs,
-        default_blocks=default_blocks,
-        input_swaps=orcasimpleinput,
-        block_swaps=orcablocks,
-        copy_files=copy_files,
-        **calc_kwargs,
+        xc=xc,
+        basis=basis,
+        orcasimpleinput=orcasimpleinput + ORCA_SIMPLE_INPUT_QUACC_IGNORE,
+        orcablocks=orcablocks,
+        nprocs=nprocs,
     )
-
-    return doc
+    # TODO: how we want to handle what results to save out and where to store
+    # them.
+    with open(os.path.join(outputdir, "quacc_results.pkl"), "wb") as f:
+        pickle.dump(o, f)
 
 
 def ase_relaxation(
@@ -104,15 +78,11 @@ def ase_relaxation(
     spin_multiplicity,
     xc=ORCA_FUNCTIONAL,
     basis=ORCA_BASIS,
-    orcasimpleinput=None,
-    orcablocks=None,
+    orcasimpleinput=ORCA_SIMPLE_INPUT,
+    orcablocks=ORCA_BLOCKS,
     nprocs=12,
-    opt_params=None,
+    opt_params=OPT_PARAMETERS,
     outputdir=os.getcwd(),
-    vertical=Vertical.Default,
-    copy_files=None,
-    nbo=False,
-    **calc_kwargs,
 ):
     """
     Wrapper around QUACC's ase_relax_job to standardize geometry optimizations.
@@ -140,46 +110,24 @@ def ase_relaxation(
         Number of processes to parallelize across
     opt_params: dict
         Dictionary of optimizer parameters
-    nbo: bool
-        Run NBO as part of the Orca calculation
     outputdir: str
         Directory to move results to upon completion
-    calc_kwargs:
-        Additional kwargs for the custom Orca calculator
     """
     from quacc import SETTINGS
 
     SETTINGS.RESULTS_DIR = outputdir
 
-    if orcasimpleinput is None:
-        orcasimpleinput = ORCA_SIMPLE_INPUT.copy()
-    if orcablocks is None:
-        orcablocks = ORCA_BLOCKS.copy()
-    if opt_params is None:
-        opt_params = OPT_PARAMETERS.copy()
-    if vertical == Vertical.MetalOrganics and spin_multiplicity == 1:
-        orcasimpleinput.append("UKS")
-        orcablocks.append(get_symm_break_block(atoms, charge))
-    if not nbo:
-        orcasimpleinput.extend(["NONBO", "NONPA"])
-    else:
-        orcablocks.append(NBO_FLAGS)
-
-    nprocs = psutil.cpu_count(logical=False) if nprocs == "max" else nprocs
-    default_inputs = [xc, basis, "engrad"]
-    default_blocks = [f"%pal nprocs {nprocs} end"]
-
-    doc = run_and_summarize_opt(
+    o = strip_decorator(ase_relax_job)(
         atoms,
         charge=charge,
         spin_multiplicity=spin_multiplicity,
-        default_inputs=default_inputs,
-        default_blocks=default_blocks,
-        input_swaps=orcasimpleinput,
-        block_swaps=orcablocks,
+        xc=xc,
+        basis=basis,
+        orcasimpleinput=orcasimpleinput + ORCA_SIMPLE_INPUT_QUACC_IGNORE,
+        orcablocks=orcablocks,
         opt_params=opt_params,
-        copy_files=copy_files,
-        **calc_kwargs,
+        nprocs=nprocs,
     )
-
-    return doc
+    # TODO: how we want to handle what results to save out and where to store them.
+    with open(os.path.join(outputdir, "quacc_results.pkl"), "wb") as f:
+        pickle.dump(o, f)
--- setup.py
+++ setup.py
@@ -13,11 +13,10 @@ setup(
     url="http://github.com/Open-Catalyst-Project/om-data",
     packages=find_packages(),
     install_requires=[
+        "ase",
+        "quacc[sella]>=0.7.2",
         "numpy",
         "tqdm",
-        "ase@git+https://gitlab.com/ase/ase.git@dc86a19a280741aa2b42a08d0fa63a8d0348e225",
-        "quacc[sella]>=0.7.6",
-        "sella==2.3.3",
     ],
     include_package_data=True,
 )
