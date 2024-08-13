23bcacf67d3ea2b2a26c49799838bbfe668cc37c
Daniel Levine <levineds@meta.com>
2024-08-13 18:04:58 +0000

    Revert "merge main"
    This reverts commit 2cc579340b08a317b194515c831eb4147960bca2, reversing
    changes made to 65b9c0b74e3b463629ff25dc15e50b71152e656a.

--- electrolytes/run_extraction.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#TODO: can we automatically extract the names of all the solute atoms from the PDB file so we don't have to re-run this command for each solute?
-
-$SCHRODINGER/run python3 solvation_shell_extract.py --input_dir 'testfiles/1' \
-                                  --save_dir 'results' \
-                                  --system_name 'Li_BF4' \
-                                  --max_frames 100
-                        
