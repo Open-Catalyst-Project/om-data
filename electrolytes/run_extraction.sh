a9b51242e4e68090e0ba3fd5aef587adab3d1a06
Sanjeev Raja <sanjeevr@germain.lbl.gov>
2024-08-23 10:07:27 -0700

    refactor and add some comments

--- electrolytes/run_extraction.sh
+++ electrolytes/run_extraction.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #TODO: can we automatically extract the names of all the solute atoms from the PDB file so we don't have to re-run this command for each solute?
 
-$SCHRODINGER/run python3 solvation_shell_extract.py --input_dir 'testfiles/1' \
+$SCHRODINGER/run python3 -m cProfile -o output100_spedup.prof solvation_shell_extract.py --input_dir 'testfiles/1' \
                                   --save_dir 'results' \
                                   --system_name 'Li_BF4' \
                                   --max_frames 100
