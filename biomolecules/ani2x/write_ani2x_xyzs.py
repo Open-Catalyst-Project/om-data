4fae39195bb3c68dc324b516804a71334289283b
Samuel M. Blau <smblau@lbl.gov>
2024-09-24 09:20:17 -0700

    Add script to write ani2x XYZ files (#34)
    * add script to write ani2x XYZ files
    
    * Update biomolecules/ani2x/write_ani2x_xyzs.py
    
    Co-authored-by: Daniel Levine <levineds@gmail.com>
    
    * Update biomolecules/ani2x/write_ani2x_xyzs.py
    
    Co-authored-by: Daniel Levine <levineds@gmail.com>
    
    ---------
    
    Co-authored-by: Samuel Blau <samuelblau@Samuels-MacBook-Pro-2.local>
    Co-authored-by: Daniel Levine <levineds@gmail.com>

---
+++ biomolecules/ani2x/write_ani2x_xyzs.py
@@ -0,0 +1,37 @@
+import argparse
+import h5py
+import numpy as np
+from ase import Atoms
+import ase.io
+from tqdm import tqdm
+from urllib.request import urlretrieve
+import os
+import tarfile
+
+def main(args):
+    tar_name = "ANI-2x-wB97X-631Gd.tar.gz"
+    hf_name = "final_h5/ANI-2x-wB97X-631Gd.h5"
+    if not os.path.exists(hf_name):
+        if not os.path.exists(tar_name):
+            urlretrieve(
+                "https://zenodo.org/records/10108942/files/ANI-2x-wB97X-631Gd.tar.gz",
+                "ANI-2x-wB97X-631Gd.tar.gz",
+            )
+        with tarfile.open(tar_name, "r:gz") as tar:
+            tar.extractall()
+    with h5py.File(hf_name) as h5:
+        for num_atoms, properties in h5.items():
+            coordinates = properties['coordinates']
+            species = properties['species']
+            for nid, (atomic_numbers, positions) in tqdm(enumerate(zip(species, coordinates))):
+                atoms = Atoms(atomic_numbers, positions=positions)
+                ase.io.write(os.path.join(args.output_path, f"ani2x_{num_atoms}_{nid}_0_1.xyz"), atoms, "xyz")
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--output_path", default=".")
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
