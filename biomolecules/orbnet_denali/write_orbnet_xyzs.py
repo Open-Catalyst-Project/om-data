67f25255739e8a65cb7138753d9c79b08628fd47
Samuel Blau <samuelblau@Samuels-MacBook-Pro-2.local>
2024-10-31 13:48:14 -0700

    Fix orbnet

--- biomolecules/orbnet_denali/write_orbnet_xyzs.py
+++ biomolecules/orbnet_denali/write_orbnet_xyzs.py
@@ -4,21 +4,25 @@ import os
 import tarfile
 from urllib.request import urlretrieve
 
-import ase.io
-import h5py
-from ase import Atoms
 from tqdm import tqdm
+import shutil
 
-
-def write_xyz(args):
-    atomic_numbers, positions, output_path = args
-    atoms = Atoms(atomic_numbers, positions=positions)
-    ase.io.write(output_path, atoms, "xyz")
-
+def copy_with_new_name(args):
+    orig_filepath, output_path, nid = args
+    split_path = orig_filepath.split("/")
+    assert len(split_path) == 3
+    specific_subdir = split_path[1]
+    file_hash = split_path[-1].split(".")[0]
+    with open(orig_filepath) as fh:
+        fh.readline()
+        line = fh.readline()
+        charge, spin = line.split()
+    new_filepath = os.path.join(output_path, f"orbnet_{specific_subdir}_{file_hash}_{nid}_{charge}_{spin}.xyz")
+    shutil.copyfile(orig_filepath, new_filepath)
 
 def main(args):
     tar_name = "denali_xyz_files.tar.gz"
-    xyz_dir_name = "denali_xyz_files"
+    xyz_dir_name = "xyz_files"
     if not os.path.exists(tar_name):
         urlretrieve(
             "https://figshare.com/ndownloader/files/28672287",
@@ -27,27 +31,15 @@ def main(args):
     if not os.path.exists(xyz_dir_name):
         with tarfile.open(tar_name, "r:gz") as tar:
             tar.extractall()
-    with h5py.File(hf_name) as h5:
-        for formula, grp in h5["data"].items():
-            for rxn, subgrp in grp.items():
-                coordinates = list(subgrp["positions"])
-                species = [
-                    [int(val) for val in list(subgrp["atomic_numbers"])]
-                    for entry in coordinates
-                ]
-                num_atoms = len(species)
+    mp_args = []
+    nid = 0
+    for subdir, dirs, files in os.walk(xyz_dir_name):
+        for file in files:
+            mp_args.append((os.path.join(subdir, file), args.output_path, nid))
+            nid += 1
 
-                mp_args = []
-                for nid, (atomic_numbers, positions) in tqdm(
-                    enumerate(zip(species, coordinates))
-                ):
-                    output_path = os.path.join(
-                        args.output_path, f"t1x_{rxn}_{num_atoms}_{nid}_0_1.xyz"
-                    )
-                    mp_args.append((atomic_numbers, positions, output_path))
-
-                pool = mp.Pool(60)
-                list(tqdm(pool.imap(write_xyz, mp_args), total=len(mp_args)))
+    pool = mp.Pool(60)
+    list(tqdm(pool.imap(copy_with_new_name, mp_args), total=len(mp_args)))
 
 
 def parse_args():
