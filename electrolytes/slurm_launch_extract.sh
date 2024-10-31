900a57dc0862a72131a0a885c9ecdffe9de3dc84
Daniel Levine <levineds@meta.com>
2024-10-31 20:24:45 +0000

    extraction with slurm

--- electrolytes/slurm_launch_extract.sh
+++ electrolytes/slurm_launch_extract.sh
@@ -10,9 +10,9 @@
 #SBATCH --mem=500m
 #SBATCH --time=72:00:00
 
-#SBATCH --array=2-4000
+#SBATCH --array=1-3757
 
 idx=$((${SLURM_ARRAY_TASK_ID}+0))
 
-echo $SCHRODINGER/run solvation_shell_extract.py --input_dir '/checkpoint/levineds/elytes_09_29_2024/'$idx --save_dir '/checkpoint/levineds/elytes_09_29_2024/results' --system_name $idx --seed $((${idx}+123)) --top_n 10
-$SCHRODINGER/run solvation_shell_extract.py --input_dir '/checkpoint/levineds/elytes_09_29_2024/'$idx --save_dir '/checkpoint/levineds/elytes_09_29_2024/results' --system_name $idx --seed $((${idx}+123)) --top_n 10
+echo $SCHRODINGER/run solvation_shell_extract.py --input_dir '/checkpoint/levineds/elytes_09_29_2024/'$idx --save_dir '/checkpoint/levineds/elytes_09_29_2024/results' --system_name $idx --seed $((${idx}+123)) --top_n 10 --max_shell_size 300 --radii 7
+$SCHRODINGER/run solvation_shell_extract.py --input_dir '/checkpoint/levineds/elytes_09_29_2024/'$idx --save_dir '/checkpoint/levineds/elytes_09_29_2024/results' --system_name $idx --seed $((${idx}+123)) --top_n 10 --max_shell_size 300 --radii 7
