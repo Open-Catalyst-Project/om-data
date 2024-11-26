84f13d7f90bf39aa6ea0aac707c3fec8390be1d4
Daniel Levine <levineds@meta.com>
2024-11-25 16:03:24 -0800

    Re-arrange parallelism for ANI-2x, Transition-1X, OrbNet-Denali, Spice (#41)

--- biomolecules/orbnet_denali/write_orbnet_xyzs.py
+++ biomolecules/orbnet_denali/write_orbnet_xyzs.py
@@ -1,11 +1,12 @@
 import argparse
 import multiprocessing as mp
 import os
+import shutil
 import tarfile
 from urllib.request import urlretrieve
 
 from tqdm import tqdm
-import shutil
+
 
 def copy_with_new_name(args):
     orig_filepath, output_path, nid = args
@@ -17,8 +18,11 @@ def copy_with_new_name(args):
         fh.readline()
         line = fh.readline()
         charge, spin = line.split()
-    new_filepath = os.path.join(output_path, f"orbnet_{specific_subdir}_{file_hash}_{nid}_{charge}_{spin}.xyz")
+    new_filepath = os.path.join(
+        output_path, f"orbnet_{specific_subdir}_{file_hash}_{nid}_{charge}_{spin}.xyz"
+    )
     shutil.copyfile(orig_filepath, new_filepath)
+
 
 def main(args):
     tar_name = "denali_xyz_files.tar.gz"
@@ -31,15 +35,17 @@ def main(args):
     if not os.path.exists(xyz_dir_name):
         with tarfile.open(tar_name, "r:gz") as tar:
             tar.extractall()
-    mp_args = []
+
+    pool = mp.Pool(60)
     nid = 0
-    for subdir, dirs, files in os.walk(xyz_dir_name):
+    os_walk = tuple(os.walk(xyz_dir_name))
+    for subdir, dirs, files in tqdm(os_walk, total=len(os_walk)):
+        mp_args = []
         for file in files:
             mp_args.append((os.path.join(subdir, file), args.output_path, nid))
             nid += 1
 
-    pool = mp.Pool(60)
-    list(tqdm(pool.imap(copy_with_new_name, mp_args), total=len(mp_args)))
+        list(tqdm(pool.imap(copy_with_new_name, mp_args), total=len(mp_args)))
 
 
 def parse_args():
--- biomolecules/spice/write_spice_xyzs.py
+++ biomolecules/spice/write_spice_xyzs.py
@@ -2,11 +2,13 @@ import argparse
 import multiprocessing as mp
 import os
 import tarfile
+from functools import partial
 from urllib.request import urlretrieve
 
 import ase.io
 import h5py
 from ase import Atoms
+from rdkit import Chem
 from tqdm import tqdm
 
 
@@ -16,6 +18,30 @@ def write_xyz(args):
     ase.io.write(output_path, atoms, "xyz")
 
 
+def work(group, hf_name):
+    with h5py.File(hf_name, swmr=True) as h5:
+        properties = h5[group]
+        subset = list(properties["subset"])[0].decode("utf-8").replace(" ", "_").replace(".", "_")
+        fixed_group = group.replace(" ", "_").replace("[", "").replace("]", "").replace(".", "_").replace(":", "")
+        coordinates = [
+            coords * 0.529177 for coords in list(properties["conformations"])
+        ]
+        species = [
+            [int(val) for val in list(properties["atomic_numbers"])]
+            for entry in coordinates
+        ]
+        mol = Chem.MolFromSmiles(list(properties["smiles"])[0])
+        charge = Chem.GetFormalCharge(mol)
+
+        mp_args = []
+        for nid, (atomic_numbers, positions) in enumerate(zip(species, coordinates)):
+            output_path = os.path.join(
+                args.output_path, f"{subset}_spice_{fixed_group}_{nid}_{charge}_1.xyz"
+            )
+            if not os.path.exists(output_path):
+                write_xyz((atomic_numbers, positions, output_path))
+
+
 def main(args):
     hf_name = "SPICE-2.0.1.hdf5"
     if not os.path.exists(hf_name):
@@ -23,32 +49,11 @@ def main(args):
             "https://zenodo.org/records/10975225/files/SPICE-2.0.1.hdf5",
             "SPICE-2.0.1.hdf5",
         )
+    pool = mp.Pool(60)
     with h5py.File(hf_name) as h5:
-        for group, properties in h5.items():
-            subset = list(properties["subset"])[0].decode("utf-8").replace(" ", "_")
-            fixed_group = group.replace(" ", "_")
-            group_output_path = os.path.join(args.output_path, subset)
-            if not os.path.exists(group_output_path):
-                os.mkdir(group_output_path)
-            coordinates = [coords*0.529177 for coords in list(properties["conformations"])]
-            species = [[int(val) for val in list(properties["atomic_numbers"])] for entry in coordinates]
-            charge = int(round(sum(list(properties["mbis_charges"])[0])[0]))
-
-            check_charges = [int(round(sum(charges)[0])) for charges in list(properties["mbis_charges"])]
-            for one_charge in check_charges:
-                assert one_charge == charge
-
-            mp_args = []
-            for nid, (atomic_numbers, positions) in tqdm(
-                enumerate(zip(species, coordinates))
-            ):
-                output_path = os.path.join(
-                    group_output_path, f"spice_{fixed_group}_{nid}_{charge}_1.xyz"
-                )
-                mp_args.append((atomic_numbers, positions, output_path))
-
-            pool = mp.Pool(60)
-            list(tqdm(pool.imap(write_xyz, mp_args), total=len(mp_args)))
+        groups = list(h5.keys())
+    work_fxn = partial(work, hf_name=hf_name)
+    list(tqdm(pool.imap(work_fxn, groups), total=len(groups)))
 
 
 def parse_args():
