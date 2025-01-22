ef3c0f05518b3bf9a18af5f8f0d7315521b867e8
Daniel Levine <levineds@meta.com>
2025-01-22 06:07:04 +0000

    nvm

--- metal-organics/slurm_launch.sh
+++ metal-organics/slurm_launch.sh
@@ -5,14 +5,13 @@
 #SBATCH --output=/private/home/levineds/architector_generation/logs/%A_%a.out
 #SBATCH --error=/private/home/levineds/architector_generation/logs/%A_%a.err
 
-#SBATCH --partition=scavenge,learnaccel,ocp
+#SBATCH --partition=scavenge
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --mem=500m
+#SBATCH --ntasks-per-node=41
+#SBATCH --mem=12g
 #SBATCH --time=72:00:00
 
-#SBATCH --array=0-18199
+#SBATCH --array=0-509
 
 export OMP_NUM_THREADS=1
-/private/home/levineds/miniconda3/envs/gpsts/bin/python mprun.py /checkpoint/levineds/arch_H/hydride_18200.pkl --n_workers 1 --batch_size 12 --batch_idx $((SLURM_ARRAY_TASK_ID+0)) --outpath /checkpoint/levineds/arch_H/inputs/
-
+/private/home/levineds/miniconda3/envs/gpsts/bin/python mprun.py /checkpoint/levineds/arch_Ln/MO_Ln_255k.pkl --n_workers 40 --batch_size 500 --batch_idx $((SLURM_ARRAY_TASK_ID+0)) --outpath /checkpoint/levineds/arch_Ln/inputs/
