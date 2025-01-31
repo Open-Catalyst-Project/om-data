a4a7bcb0cdc6dce64e599238bb3dcb25be5f291c
Muhammad Risyad Hasyim <muhammadhasyim@github.com>
2025-01-31 14:28:53 -0500

    Update the workflow OpenMM script

--- electrolytes/runsim_omm_rpmd.sh
+++ electrolytes/runsim_omm_rpmd.sh
@@ -3,9 +3,7 @@
 num_lines=$(wc -l < rpmd_elytes.csv)
 num_lines=$((num_lines-1))
 
-# Define the numbers as a space-separated list
 # Loop through each number
-# Test with the first 10 systems in elytes.csv for now!
 for ((i = 1; i < $num_lines; i++)); do
     rm $i -rf
     mkdir $i
@@ -13,13 +11,14 @@ for ((i = 1; i < $num_lines; i++)); do
     
     #Generate and run short simulation of the solvent 
     python generatesolvent_omm.py $i
-    python runsolvent.py --job_dir=./ --row_idx=$i --rpmd --nbeads=10
+    python runsolvent.py --job_dir=./ --row_idx=$i --rpmd --nbeads=32
     
-    #Generate the system and relax
+    #Generate the system and energy-minimize
     python generatesystem_omm.py $i 
     cd $i
     cp ../relaxsystem.py ./
     python relaxsystem.py $i
     cd ..
-    python runsystem.py --job_dir=./ --row_idx=$i --rpmd --nbeads=10
+    #Run the MD simulation
+    python runsystem.py --job_dir=./ --row_idx=$i --rpmd --nbeads=32
 done
