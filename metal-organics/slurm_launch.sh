04588d8ac681e8a2a36340ade906efa138987cf6
Daniel Levine <levineds@meta.com>
2025-02-21 15:38:51 -0800

    MO changes for ML-MD sampling (#49)

--- metal-organics/slurm_launch.sh
+++ metal-organics/slurm_launch.sh
@@ -5,13 +5,13 @@
 #SBATCH --output=/private/home/levineds/architector_generation/logs/%A_%a.out
 #SBATCH --error=/private/home/levineds/architector_generation/logs/%A_%a.err
 
-#SBATCH --partition=scavenge
+#SBATCH --partition=scavenge,learnaccel
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=41
 #SBATCH --mem=12g
 #SBATCH --time=72:00:00
 
-#SBATCH --array=0-509
+#SBATCH --array=0-3999
 
 export OMP_NUM_THREADS=1
-/private/home/levineds/miniconda3/envs/gpsts/bin/python mprun.py /checkpoint/levineds/arch_Ln/MO_Ln_255k.pkl --n_workers 40 --batch_size 500 --batch_idx $((SLURM_ARRAY_TASK_ID+0)) --outpath /checkpoint/levineds/arch_Ln/inputs/
+/private/home/levineds/miniconda3/envs/gpsts/bin/python mprun.py /checkpoint/levineds/ml_md/metal_organics/metal_organics_MS_1M.pkl --n_workers 40 --batch_size 250 --batch_idx $((SLURM_ARRAY_TASK_ID+0)) --outpath /checkpoint/levineds/ml_md/metal_organics/inputs/
