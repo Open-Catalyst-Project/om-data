3be5d0a8557dd3df837db3d03839e6ffa7c3a237
Daniel Levine <levineds@meta.com>
2025-03-06 19:39:31 +0000

    Debugging with radical and non-radical example

--- reactivity/convert_smirks_to_reaction_smarts.py
+++ reactivity/convert_smirks_to_reaction_smarts.py
@@ -6,6 +6,7 @@ import itertools
 
 smirks = 'CC(C)(C[O:11][N+:10]([O-])=O)C.[Ar]>>CC(C)(C[O:11])C.[O-][N+:10]=O.[Ar] 10,11-10;10,11-11'
 smirks = 'C1=C[C:63]2=[C:64](C=C1)[CH:73]=[CH:72][C:61]([S-:10])=[CH:62]2.Cl[C:34]1=[CH:33][CH:32]=[C:31]([CH+:20][C:41]2=[CH:42][CH:43]=[C:44](Cl)[CH:45]=[CH:46]2)[CH:36]=[CH:35]1>>Cl[C:34]1=[CH:33][CH:32]=[C:31]([CH:20]([S:10][C:61]2=[CH:62][C:63]3=[C:64](C=CC=C3)[CH:73]=[CH:72]2)[C:41]2=[CH:42][CH:43]=[C:44](Cl)[CH:45]=[CH:46]2)[CH:36]=[CH:35]1 10=20'
+USE_SMILES=False
 
 def convert_smirks_to_reaction_smarts(smirks):
     # Remove H labels on mapped atoms to make SMARTS more promiscuous
@@ -17,7 +18,7 @@ def convert_smirks_to_reaction_smarts(smirks):
     # Kekulize reactants and products
     AllChem.SanitizeRxnAsMols(rxn)
     # Round-trip the reaction to make SMARTS patterns include aromaticity in atom labels
-    rxn = AllChem.ReactionFromSmarts(AllChem.ReactionToSmiles(rxn), useSmiles=True)
+    rxn = AllChem.ReactionFromSmarts(AllChem.ReactionToSmiles(rxn), useSmiles=USE_SMILES)
 
     # Remove the unmapped atoms
     new_rs = []
@@ -41,7 +42,7 @@ def convert_smirks_to_reaction_smarts(smirks):
     # Do both the forward and back reaction
     for r, p in itertools.permutations([r_smarts, p_smarts]):
         new_smarts = '.'.join(r) + '>>' + '.'.join(p)
-        rxn = AllChem.ReactionFromSmarts(new_smarts, useSmiles=True)
+        rxn = AllChem.ReactionFromSmarts(new_smarts, useSmiles=USE_SMILES)
         rxn_copy = copy(rxn)
         AllChem.RemoveMappingNumbersFromReactions(rxn_copy)
         rxn_key = AllChem.ReactionToSmarts(rxn_copy)
@@ -51,3 +52,10 @@ new_reactions = convert_smirks_to_reaction_smarts(smirks)
 for key, val in new_reactions.items():
     print(key)
     print(AllChem.ReactionToSmarts(val))
+    #ps = val.RunReactants([Chem.MolFromSmiles('CO[N+](C)(C)CC')])
+    #Chem.SanitizeMol(ps[0][1])
+    #print(Chem.MolToSmiles(ps[0][1]))
+    ps = val.RunReactants([Chem.MolFromSmiles('[S-]c1ccccc1'), Chem.MolFromSmiles('c1ccccc1([CH+]c1ccccc1)')])
+    print(ps)
+    Chem.SanitizeMol(ps[0][0])
+    print(Chem.MolToSmiles(ps[0][0]))
