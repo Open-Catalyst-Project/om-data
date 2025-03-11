3378ad5cafd9904079bcff78d8e1adcbb73ed996
Muhammed Shuaibi <mushuaibi@gmail.com>
2025-03-11 18:33:54 +0000

    minor changes

--- reactivity/electrolyte_reactivity/popcornn_elytes.py
+++ reactivity/electrolyte_reactivity/popcornn_elytes.py
@@ -1,18 +1,21 @@
 # pip install popcornn
-from popcornn import tools, optimize_MEP
-from ase.io import read,write
-import os
 import argparse
+import os
+
+import numpy as np
+from ase.io import read, write
+from popcornn import optimize_MEP, tools
+from tqdm import tqdm
+
 
 def main(args):
     input_path = args.input_path
     output_path = args.output_path
-    start_index = args.start_index
-    end_index = args.end_index
+    indices = np.array_split(range(0, 146961), args.slurm_workers)[args.chunk]
 
     os.makedirs(output_path, exist_ok=True)
 
-    for i in range(start_index, end_index):
+    for i in tqdm(indices):
         print(f"Processing reaction {i}")
         lines = open(f"{input_path}/{i}_R.xyz").readlines()
         charge = lines[1].split()[0]
@@ -29,13 +32,15 @@ def main(args):
                 continue
             write(f"{output_path}/{i}_{j}_{charge}_{spin_multiplicity}.xyz", atoms)
 
+
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input_path", default=".")
-    parser.add_argument("--output_path", default=".")
-    parser.add_argument("--start_index", type=int)
-    parser.add_argument("--end_index", type=int)
+    parser.add_argument("--input_path")
+    parser.add_argument("--output_path")
+    parser.add_argument("--chunk", type=int)
+    parser.add_argument("--slurm-workers", type=int)
     return parser.parse_args()
+
 
 if __name__ == "__main__":
     args = parse_args()
