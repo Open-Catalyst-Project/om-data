2889fe5baaa0510a432b2f48f3f0dd0a28cf22ae
samblau <samuelblau@Samuels-MBP-2.dhcp.lbl.gov>
2025-03-12 11:05:36 -0700

    Change first two mlip evals to use dicts, tqdm

--- omdata/eval_workflows/mlip_evals.py
+++ omdata/eval_workflows/mlip_evals.py
@@ -4,6 +4,7 @@ from ase.atoms import Atoms
 from quacc.recipes.orca._base import prep_calculator
 import os
 import psutil
+import tqdm
 from quacc import job
 from quacc.runners.ase import Runner
 from quacc.schemas.ase import Summarize, VibSummarize
@@ -23,37 +24,39 @@ from omdata.orca.calc import (
     get_symm_break_block,
 )
 
-from omdata.eval_workflows.eval_workflows import ase_calc_single_point_job, ase_calc_freq_job, ase_calc_relax_job
+from omdata.eval_workflows.eval_jobs import ase_calc_single_point_job, ase_calc_relax_job
 
-# Each entry in this list should be a three element list with ligand_pocket structure, ligand structure, pocket structure
-ligand_pocket_structures = []
+# key: identifier, value: {"ligand_pocket": {"atoms": Atoms, "charge": int, "spin_multiplicity": int}, "ligand": {"atoms": Atoms, "charge": int, "spin_multiplicity": int}, "pocket": {"atoms": Atoms, "charge": int, "spin_multiplicity": int}}
+ligand_pocket_structures = {}
 
-# Each entry in this list should be an N element list of the N different conformer structures for a given family
-geom_conformers_structures = []
+# key: family_identifier, value: {"conformer_identifier": {"atoms": Atoms, "charge": int, "spin_multiplicity": int}, ...}
+geom_conformers_structures = {}
 
 def mlip_ligand_pocket(ligand_pocket_structures):
     results = {}
-    for ii, structs in enumerate(ligand_pocket_structures):
-        lp_calc = prep_mlip_calc(structs[0]["charge"], structs[0]["spin_multiplicity"])
-        l_calc = prep_mlip_calc(structs[1]["charge"], structs[1]["spin_multiplicity"])
-        p_calc = prep_mlip_calc(structs[2]["charge"], structs[2]["spin_multiplicity"])
+    for identifier, structs in tqdm.tqdm(ligand_pocket_structures.items()):
+        lp_calc = prep_mlip_calc(structs["ligand_pocket"]["charge"], structs["ligand_pocket"]["spin_multiplicity"])
+        l_calc = prep_mlip_calc(structs["ligand"]["charge"], structs["ligand"]["spin_multiplicity"])
+        p_calc = prep_mlip_calc(structs["pocket"]["charge"], structs["pocket"]["spin_multiplicity"])
 
-        lp_result = ase_calc_single_point_job(lp_calc, structs[0]["atoms"], structs[0]["charge"], structs[0]["spin_multiplicity"])
-        l_result = ase_calc_single_point_job(l_calc, structs[1]["atoms"], structs[1]["charge"], structs[1]["spin_multiplicity"])
-        p_result = ase_calc_single_point_job(p_calc, structs[2]["atoms"], structs[2]["charge"], structs[2]["spin_multiplicity"])
+        lp_result = ase_calc_single_point_job(lp_calc, structs["ligand_pocket"]["atoms"], structs["ligand_pocket"]["charge"], structs["ligand_pocket"]["spin_multiplicity"])
+        l_result = ase_calc_single_point_job(l_calc, structs["ligand"]["atoms"], structs["ligand"]["charge"], structs["ligand"]["spin_multiplicity"])
+        p_result = ase_calc_single_point_job(p_calc, structs["pocket"]["atoms"], structs["pocket"]["charge"], structs["pocket"]["spin_multiplicity"])
 
-        results[ii] = [lp_result, l_result, p_result]
+        results[identifier] = {"ligand_pocket": lp_result, "ligand": l_result, "pocket": p_result}
 
     dumpfn(results, "mlip_ligand_pocket.json")
 
 def mlip_geom_conformers(geom_conformers_structures):
     results = {}
-    for ii, structs in enumerate(geom_conformers_structures):
+    for family_identifier, structs in tqdm.tqdm(geom_conformers_structures.items()):
         family_results = {}
-        for jj, struct in enumerate(structs):
+        for conformer_identifier, struct in structs.items():
             calc = prep_mlip_calc(struct["charge"], struct["spin_multiplicity"])
             result = ase_calc_relax_job(calc, struct["atoms"], struct["charge"], struct["spin_multiplicity"])
-            family_results[jj] = result
-        results[ii] = family_results
+            family_results[conformer_identifier] = result
+        results[family_identifier] = family_results
 
     dumpfn(results, "mlip_geom_conformers.json")
+
+
