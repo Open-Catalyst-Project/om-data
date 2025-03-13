a8231134fe26641b3cab34bb455cbefec9cdde71
samblau <samuelblau@Samuels-MacBook-Pro-2.local>
2025-03-13 14:35:16 -0700

    Simplify/modularize code

--- omdata/eval_workflows/evaluator.py
+++ omdata/eval_workflows/evaluator.py
@@ -10,6 +10,48 @@ def cosine_similarity(forces_1, forces_2):
     return np.dot(forces_1, forces_2) / (np.linalg.norm(forces_1) * np.linalg.norm(forces_2))
 
 
+def interaction_energy_and_forces(results, principal_identifier):
+    interaction_energy = {}
+    interaction_forces = {}
+    for identifier in results.keys():
+        non_principal_forces = None
+        non_principal_energy = 0
+        for component_identifier in results[identifier].keys():
+            if component_identifier != principal_identifier:
+                non_principal_energy += results[identifier][component_identifier]["energy"]
+                if non_principal_forces is None:
+                    non_principal_forces = results[identifier][component_identifier]["forces"]
+                else: # Note that the following concatenation assumes that the principal atoms are in the order of each component in sequence
+                    non_principal_forces = np.concatenate((non_principal_forces, results[identifier][component_identifier]["forces"]))
+        interaction_energy[identifier] = results[identifier][principal_identifier]["energy"] - non_principal_energy
+        interaction_forces[identifier] = results[identifier][principal_identifier]["forces"] - non_principal_forces
+    return interaction_energy, interaction_forces
+
+
+def spin_deltas(results):
+    deltaE = {}
+    deltaF = {}
+    for identifier in results.keys():
+        deltaE[identifier] = results[identifier]["high_spin"]["energy"] - results[identifier]["low_spin"]["energy"]
+        deltaF[identifier] = results[identifier]["high_spin"]["forces"] - results[identifier]["low_spin"]["forces"]
+    return deltaE, deltaF
+
+
+def charge_deltas(results):
+    deltaE = {}
+    deltaF = {}
+    for identifier in results.keys():
+        deltaE[identifier] = {"add_electron": {}, "remove_electron": {}}
+        deltaF[identifier] = {"add_electron": {}, "remove_electron": {}}
+        orig_energy = results[identifier]["original"][results[identifier]["original"].keys()[0]]["energy"]
+        orig_forces = results[identifier]["original"][results[identifier]["original"].keys()[0]]["forces"]
+        for tag in ["add_electron", "remove_electron"]:
+            for spin_multiplicity in results[identifier][tag].keys():
+                deltaE[identifier][tag][spin_multiplicity] = results[identifier][tag][spin_multiplicity]["energy"] - orig_energy
+                deltaF[identifier][tag][spin_multiplicity] = results[identifier][tag][spin_multiplicity]["forces"] - orig_forces
+    return deltaE, deltaF
+
+
 # The use of task_metrics and eval are both mockups and will need help to function as envisioned
 class OMol_Evaluator:
     task_metrics = {
@@ -65,19 +107,16 @@ class OMol_Evaluator:
         interaction_energy_mae = 0
         interaction_forces_mae = 0
         interaction_forces_cosine_similarity = 0
+        orca_interaction_energy, orca_interaction_forces = interaction_energy_and_forces(orca_results, "ligand_pocket")
+        mlip_interaction_energy, mlip_interaction_forces = interaction_energy_and_forces(mlip_results, "ligand_pocket")
         for identifier in orca_results.keys():
             for component_identifier in orca_results[identifier].keys():
                 energy_mae += abs(orca_results[identifier][component_identifier]["energy"] - mlip_results[identifier][component_identifier]["energy"])
                 forces_mae += np.mean(np.abs(orca_results[identifier][component_identifier]["forces"] - mlip_results[identifier][component_identifier]["forces"]))
                 forces_cosine_similarity += cosine_similarity(orca_results[identifier][component_identifier]["forces"], mlip_results[identifier][component_identifier]["forces"])
-            orca_interaction_energy = orca_results[identifier]["ligand_pocket"]["energy"] - orca_results[identifier]["ligand"]["energy"] - orca_results[identifier]["pocket"]["energy"]
-            mlip_interaction_energy = mlip_results[identifier]["ligand_pocket"]["energy"] - mlip_results[identifier]["ligand"]["energy"] - mlip_results[identifier]["pocket"]["energy"]
-            interaction_energy_mae += abs(orca_interaction_energy - mlip_interaction_energy)
-            # Note that the following concatenation assumes that ligand_pocket atom indices are in the order of ligand followed by pocket
-            orca_interaction_forces = orca_results[identifier]["ligand_pocket"]["forces"] - np.concatenate((orca_results[identifier]["ligand"]["forces"], orca_results[identifier]["pocket"]["forces"]))
-            mlip_interaction_forces = mlip_results[identifier]["ligand_pocket"]["forces"] - np.concatenate((mlip_results[identifier]["ligand"]["forces"], mlip_results[identifier]["pocket"]["forces"]))
-            interaction_forces_mae += np.mean(np.abs(orca_interaction_forces - mlip_interaction_forces))
-            interaction_forces_cosine_similarity += cosine_similarity(orca_interaction_forces, mlip_interaction_forces)
+            interaction_energy_mae += abs(orca_interaction_energy[identifier] - mlip_interaction_energy[identifier])
+            interaction_forces_mae += np.mean(np.abs(orca_interaction_forces[identifier] - mlip_interaction_forces[identifier]))
+            interaction_forces_cosine_similarity += cosine_similarity(orca_interaction_forces[identifier], mlip_interaction_forces[identifier])
         results = {
             "energy": {"mae": energy_mae / len(orca_results.keys())},
             "forces": {"mae": forces_mae / len(orca_results.keys()), "cosine_similarity": forces_cosine_similarity / len(orca_results.keys())},
@@ -106,34 +145,18 @@ class OMol_Evaluator:
         deltaE_mae = 0
         deltaF_mae = 0
         deltaF_cosine_similarity = 0
+        orca_deltaE, orca_deltaF = charge_deltas(orca_results)
+        mlip_deltaE, mlip_deltaF = charge_deltas(mlip_results)
         for identifier in orca_results.keys():
             for tag in ["original", "add_electron", "remove_electron"]:
                 for spin_multiplicity in orca_results[identifier][tag].keys():
-                    energy_mae += abs(orca_results[identifier][tag][spin_multiplicity]["energy"] - ml_results[identifier][tag][spin_multiplicity]["energy"])
-                    forces_mae += np.mean(np.abs(orca_results[identifier][tag][spin_multiplicity]["forces"] - ml_results[identifier][tag][spin_multiplicity]["forces"]))
+                    energy_mae += abs(orca_results[identifier][tag][spin_multiplicity]["energy"] - mlip_results[identifier][tag][spin_multiplicity]["energy"])
+                    forces_mae += np.mean(np.abs(orca_results[identifier][tag][spin_multiplicity]["forces"] - mlip_results[identifier][tag][spin_multiplicity]["forces"]))
                     forces_cosine_similarity += cosine_similarity(orca_results[identifier][tag][spin_multiplicity]["forces"], ml_results[identifier][tag][spin_multiplicity]["forces"])
-            
-            orca_orig_energy = orca_results[identifier]["original"][orca_results[identifier]["original"].keys()[0]]["energy"]
-            orca_orig_forces = orca_results[identifier]["original"][orca_results[identifier]["original"].keys()[0]]["forces"]
-            mlip_orig_energy = ml_results[identifier]["original"][mlip_results[identifier]["original"].keys()[0]]["energy"]
-            mlip_orig_forces = ml_results[identifier]["original"][mlip_results[identifier]["original"].keys()[0]]["forces"]
-            for spin_multiplicity in orca_results[identifier]["add_electron"].keys():
-                orca_deltaE = orca_results[identifier]["add_electron"][spin_multiplicity]["energy"] - orca_orig_energy
-                orca_deltaF = orca_results[identifier]["add_electron"][spin_multiplicity]["forces"] - orca_orig_forces
-                mlip_deltaE = ml_results[identifier]["add_electron"][spin_multiplicity]["energy"] - mlip_orig_energy
-                mlip_deltaF = ml_results[identifier]["add_electron"][spin_multiplicity]["forces"] - mlip_orig_forces
-                deltaE_mae += abs(orca_deltaE - mlip_deltaE)
-                deltaF_mae += np.mean(np.abs(orca_deltaF - mlip_deltaF))
-                deltaF_cosine_similarity += cosine_similarity(orca_deltaF, mlip_deltaF)
-
-            for spin_multiplicity in orca_results[identifier]["remove_electron"].keys():
-                orca_deltaE = orca_results[identifier]["remove_electron"][spin_multiplicity]["energy"] - orca_orig_energy
-                orca_deltaF = orca_results[identifier]["remove_electron"][spin_multiplicity]["forces"] - orca_orig_forces
-                mlip_deltaE = ml_results[identifier]["remove_electron"][spin_multiplicity]["energy"] - mlip_orig_energy
-                mlip_deltaF = ml_results[identifier]["remove_electron"][spin_multiplicity]["forces"] - mlip_orig_forces
-                deltaE_mae += abs(orca_deltaE - mlip_deltaE)
-                deltaF_mae += np.mean(np.abs(orca_deltaF - mlip_deltaF))
-                deltaF_cosine_similarity += cosine_similarity(orca_deltaF, mlip_deltaF)
+                    if tag != "original":
+                        deltaE_mae += abs(orca_deltaE[identifier][tag][spin_multiplicity] - mlip_deltaE[identifier][tag][spin_multiplicity])
+                        deltaF_mae += np.mean(np.abs(orca_deltaF[identifier][tag][spin_multiplicity] - mlip_deltaF[identifier][tag][spin_multiplicity]))
+                        deltaF_cosine_similarity += cosine_similarity(orca_deltaF[identifier][tag][spin_multiplicity], mlip_deltaF[identifier][tag][spin_multiplicity])
 
         results = {
             "energy": {"mae": energy_mae / len(orca_results.keys())},
@@ -142,7 +165,7 @@ class OMol_Evaluator:
             "deltaF": {"mae": deltaF_mae / len(orca_results.keys()), "cosine_similarity": deltaF_cosine_similarity / len(orca_results.keys())}
         }
         return results
-            
+
 
     def distance_scaling(self, orca_results, mlip_results):
         energy_mae = 0
@@ -151,31 +174,16 @@ class OMol_Evaluator:
         interaction_energy_mae = 0
         interaction_forces_mae = 0
         interaction_forces_cosine_similarity = 0
+        orca_interaction_energy, orca_interaction_forces = interaction_energy_and_forces(orca_results, "complex")
+        mlip_interaction_energy, mlip_interaction_forces = interaction_energy_and_forces(mlip_results, "complex")
         for identifier in orca_results.keys():
-            orca_non_complex_forces = None
-            orca_non_complex_energy = 0
-            mlip_non_complex_forces = None
-            mlip_non_complex_energy = 0
             for component_identifier in orca_results[identifier].keys():
                 energy_mae += abs(orca_results[identifier][component_identifier]["energy"] - mlip_results[identifier][component_identifier]["energy"])
                 forces_mae += np.mean(np.abs(orca_results[identifier][component_identifier]["forces"] - mlip_results[identifier][component_identifier]["forces"]))
                 forces_cosine_similarity += cosine_similarity(orca_results[identifier][component_identifier]["forces"], mlip_results[identifier][component_identifier]["forces"])
-                if component_identifier != "complex":
-                    orca_non_complex_energy += orca_results[identifier][component_identifier]["energy"]
-                    mlip_non_complex_energy += mlip_results[identifier][component_identifier]["energy"]
-                    if orca_non_complex_forces is None:
-                        orca_non_complex_forces = orca_results[identifier][component_identifier]["forces"]
-                        mlip_non_complex_forces = mlip_results[identifier][component_identifier]["forces"]
-                    else: # Note that the following concatenation assumes that the complex atoms are in the order of each component in sequence
-                        orca_non_complex_forces = np.concatenate((orca_non_complex_forces, orca_results[identifier][component_identifier]["forces"]))
-                        mlip_non_complex_forces = np.concatenate((mlip_non_complex_forces, mlip_results[identifier][component_identifier]["forces"]))
-            orca_interaction_energy = orca_results[identifier]["complex"]["energy"] - orca_non_complex_energy
-            mlip_interaction_energy = mlip_results[identifier]["complex"]["energy"] - mlip_non_complex_energy
-            orca_interaction_forces = orca_results[identifier]["complex"]["forces"] - orca_non_complex_forces
-            mlip_interaction_forces = mlip_results[identifier]["complex"]["forces"] - mlip_non_complex_forces
-            interaction_energy_mae += abs(orca_interaction_energy - mlip_interaction_energy)
-            interaction_forces_mae += np.mean(np.abs(orca_interaction_forces - mlip_interaction_forces))
-            interaction_forces_cosine_similarity += cosine_similarity(orca_interaction_forces, mlip_interaction_forces)
+            interaction_energy_mae += abs(orca_interaction_energy[identifier] - mlip_interaction_energy[identifier])
+            interaction_forces_mae += np.mean(np.abs(orca_interaction_forces[identifier] - mlip_interaction_forces[identifier]))
+            interaction_forces_cosine_similarity += cosine_similarity(orca_interaction_forces[identifier], mlip_interaction_forces[identifier])
 
         results = {
             "energy": {"mae": energy_mae / len(orca_results.keys())},
@@ -193,18 +201,16 @@ class OMol_Evaluator:
         deltaE_mae = 0
         deltaF_mae = 0
         deltaF_cosine_similarity = 0
+        orca_deltaE, orca_deltaF = spin_deltas(orca_results)
+        mlip_deltaE, mlip_deltaF = spin_deltas(mlip_results)
         for identifier in orca_results.keys():
             for key in ["high_spin", "low_spin"]:
                 energy_mae += abs(orca_results[identifier][key]["energy"] - mlip_results[identifier][key]["energy"])
                 forces_mae += np.mean(np.abs(orca_results[identifier][key]["forces"] - mlip_results[identifier][key]["forces"]))
                 forces_cosine_similarity += cosine_similarity(orca_results[identifier][key]["forces"], mlip_results[identifier][key]["forces"])
-            orca_deltaE = orca_results[identifier]["high_spin"]["energy"] - orca_results[identifier]["low_spin"]["energy"]
-            mlip_deltaE = mlip_results[identifier]["high_spin"]["energy"] - mlip_results[identifier]["low_spin"]["energy"]
-            deltaE_mae += abs(orca_deltaE - mlip_deltaE)
-            orca_deltaF = orca_results[identifier]["high_spin"]["forces"] - orca_results[identifier]["low_spin"]["forces"]
-            mlip_deltaF = mlip_results[identifier]["high_spin"]["forces"] - mlip_results[identifier]["low_spin"]["forces"]
-            deltaF_mae += np.mean(np.abs(orca_deltaF - mlip_deltaF))
-            deltaF_cosine_similarity += cosine_similarity(orca_deltaF, mlip_deltaF)
+            deltaE_mae += abs(orca_deltaE[identifier] - mlip_deltaE[identifier])
+            deltaF_mae += np.mean(np.abs(orca_deltaF[identifier] - mlip_deltaF[identifier]))
+            deltaF_cosine_similarity += cosine_similarity(orca_deltaF[identifier], mlip_deltaF[identifier])
 
         results = {
             "energy": {"mae": energy_mae / len(orca_results.keys())},
