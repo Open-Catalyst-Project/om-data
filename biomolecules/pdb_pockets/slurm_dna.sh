61866a48379530b1847063f15c2e460f5bc79d58
Daniel Levine <levineds@meta.com>
2025-03-21 22:36:01 +0000

    updates to slurm launch scripts for DNA/RNA

--- biomolecules/pdb_pockets/slurm_dna.sh
+++ biomolecules/pdb_pockets/slurm_dna.sh
@@ -5,7 +5,7 @@
 #SBATCH --output=/checkpoint/levineds/logs/%A_%a.out
 #SBATCH --error=/checkpoint/levineds/logs/%A_%a.err
 
-#SBATCH --partition=scavenge
+#SBATCH --partition=scavenge,learnaccel,ocp
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=500m
@@ -13,4 +13,4 @@
 #SBATCH --array=0-1506
 
 val=$SLURM_ARRAY_TASK_ID
-/private/home/levineds/schrodinger2024-2/run na_extraction.py --start_idx $((val*5)) --end_idx $((val*5+5)) --output_path /checkpoint/levineds/dna/ --seed $val
+/private/home/levineds/schrodinger2024-2/run /private/home/levineds/om-data/biomolecules/pdb_pockets/na_extraction.py --start_idx $((val*5)) --end_idx $((val*5+5)) --output_path /checkpoint/levineds/dna/prepped --seed $val
